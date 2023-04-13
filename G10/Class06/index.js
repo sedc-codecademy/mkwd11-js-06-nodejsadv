c9157e096eeee22dcb95e458c99283b3d88fa087
Ivo Kostovski <ivo.kostovski@gmail.com>
2023-04-13 19:43:13 +0200

    class 6 break 2

--- G10/Class06/index.js
+++ G10/Class06/index.js
@@ -41,5 +41,74 @@ var Colors;
     Colors[Colors["red"] = 3] = "red"; // 3
 })(Colors || (Colors = {}));
 console.log(Colors.green);
-// colors = white is 0
 console.log(Colors.white === Colors.white);
+// Arrays
+// Option 1
+var numbers = [1, 2, 3];
+// Option 2
+var numbersAgain = [1, 2, 3];
+// Tuple
+var employees = [1, 'Ivo'];
+// Any - Use with caution!!!
+var randomStuff = [true, [], 'nekoj string', 1];
+// Interfaces
+// interface Animal {
+//     name: string;
+//     age: number
+// }
+// let animal: Animal = {
+//     name: 'Sparky',
+//     age: 23
+// }
+// let newAnimal = {
+//     name: 'Blacky',
+//     age: 10
+// } satisfies Animal;
+// Types
+// type Animal = {
+//     name: string;
+//     age: number;
+// }
+// type Nullish = undefined | null;
+// let nothing: Nullish = undefined;
+// Objects
+// interface Person {
+//     name: string;
+//     age: number;
+// }
+// let emptyObject = {} as Person;
+// let test = 'black' as any as Person;
+// interface Employee extends Person {
+//     job: string;
+// }
+// const newEmployee: Employee = {
+//     name: 'John',
+//     age: 20, 
+//     job: 'teacher'
+// }
+// Other types
+var anything = 'sdasda';
+var mistery;
+// void
+var func = function () {
+    console.log('nesto');
+};
+var func2 = function () {
+    throw new Error('Some error');
+};
+var sumOfTwo = function (number1, number2) {
+    return number1 + number2;
+};
+var sum = sumOfTwo(1, 2);
+// sumOfTwo('1', '2')
+console.log(sum);
+var sumOfItems = function (item1, item2) {
+    // console.log(typeof item1)
+    // console.log(typeof item2)
+    // return item1 + item2
+    if (typeof item1 === 'number' && typeof item2 === 'number') {
+        return item1 + item2;
+    }
+    return "".concat(item1).concat(item2);
+};
+console.log(sumOfItems(1, '2'));
--- G10/Class06/index.ts
+++ G10/Class06/index.ts
@@ -52,3 +52,106 @@ enum Colors {
 
 console.log(Colors.green)
 console.log(Colors.white === Colors.white)
+
+// Arrays
+
+// Option 1
+let numbers: number[] = [1, 2, 3]
+
+// Option 2
+let numbersAgain: Array<number> = [1, 2, 3]
+
+// Tuple
+let employees: [number, string] = [1, 'Ivo']
+
+// Any - Use with caution!!!
+let randomStuff: any[] = [true, [], 'nekoj string', 1 ]
+
+// Interfaces
+
+// interface Animal {
+//     name: string;
+//     age: number
+// }
+
+// let animal: Animal = {
+//     name: 'Sparky',
+//     age: 23
+// }
+
+// let newAnimal = {
+//     name: 'Blacky',
+//     age: 10
+// } satisfies Animal;
+
+// Types
+
+// type Animal = {
+//     name: string;
+//     age: number;
+// }
+
+// type Nullish = undefined | null;
+
+// let nothing: Nullish = undefined;
+
+// Objects
+
+// interface Person {
+//     name: string;
+//     age: number;
+// }
+
+// let emptyObject = {} as Person;
+
+// let test = 'black' as any as Person;
+
+// interface Employee extends Person {
+//     job: string;
+// }
+
+// const newEmployee: Employee = {
+//     name: 'John',
+//     age: 20, 
+//     job: 'teacher'
+// }
+
+// Other types
+
+let anything: any = 'sdasda';
+
+let mistery: unknown;
+
+// void
+
+const func = (): void => {
+    console.log('nesto')
+}
+
+const func2 = (): never => {
+    throw new Error('Some error')
+}
+
+const sumOfTwo = (number1: number, number2: number): number => {
+    return number1 + number2
+}
+
+const sum = sumOfTwo(1, 2)
+
+// sumOfTwo('1', '2')
+
+console.log(sum)
+
+const sumOfItems = (item1: number | string, item2: number | string): any => {
+    // console.log(typeof item1)
+    // console.log(typeof item2)
+    // return item1 + item2
+
+    if (typeof item1 === 'number' && typeof item2 === 'number') {
+        return item1 + item2
+    }
+
+    return `${item1}${item2}`
+}
+
+console.log(sumOfItems(1, '2'))
