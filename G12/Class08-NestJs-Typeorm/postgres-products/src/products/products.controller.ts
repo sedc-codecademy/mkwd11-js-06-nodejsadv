cf2eafb0620a505724e6a283ff4ce697a47d8ed6
borisovski-borche <75560716+borisovski-borche@users.noreply.github.com>
2023-04-19 20:32:21 +0200

    finished postgres products

--- G12/Class08-NestJs-Typeorm/postgres-products/src/products/products.controller.ts
+++ G12/Class08-NestJs-Typeorm/postgres-products/src/products/products.controller.ts
@@ -1,15 +1,35 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { UpdateProductDto } from './dtos/update-product.dto';
+import { ProductFilters } from './interfaces/products-filters.interface';
 
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
   @Get()
-  getAllProducts() {
-    return this.productsService.findAllProducts();
+  getAllProducts(
+    @Query('title') title: string,
+    @Query('inStock') inStock: string,
+    @Query('orderBy') orderBy: 'stock' | 'price',
+  ) {
+    const filters: ProductFilters = {
+      title,
+      inStock: !!inStock,
+      orderBy,
+    };
+
+    return this.productsService.findAllProducts(filters);
   }
 
   @Get('/:id')
@@ -28,5 +48,10 @@ export class ProductsController {
     @Body() updateData: UpdateProductDto,
   ) {
     return this.productsService.updateProduct(Number(productId), updateData);
+  }
+
+  @Delete('/:id')
+  deleteProduct(@Param('id') productId: string) {
+    return this.productsService.removeProduct(Number(productId));
   }
 }
--- G12/Class08-NestJs-Typeorm/postgres-products/src/products/products.service.ts
+++ G12/Class08-NestJs-Typeorm/postgres-products/src/products/products.service.ts
@@ -1,16 +1,29 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './product.entity';
-import { Repository } from 'typeorm';
+import { FindManyOptions, MoreThan, Repository } from 'typeorm';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { UpdateProductDto } from './dtos/update-product.dto';
+import { ProductFilters } from './interfaces/products-filters.interface';
 
 @Injectable()
 export class ProductsService {
   @InjectRepository(Product) private productsRepo: Repository<Product>;
 
-  findAllProducts() {
-    return this.productsRepo.find({});
+  findAllProducts(filters: ProductFilters) {
+    const filterConfig: FindManyOptions<Product> = {};
+
+    if (filters.title)
+      filterConfig.where = { ...filterConfig.where, title: filters.title };
+
+    if (filters.inStock)
+      filterConfig.where = { ...filterConfig.where, stock: MoreThan(0) };
+
+    if (filters.orderBy) {
+      if (filters.orderBy === 'stock') filterConfig.order = { stock: 'ASC' };
+      if (filters.orderBy === 'price') filterConfig.order = { price: 'ASC' };
+    }
+    return this.productsRepo.find(filterConfig);
   }
 
   async findProductById(id: number) {
@@ -35,5 +48,11 @@ export class ProductsService {
     Object.assign(product, updateData);
 
     await this.productsRepo.save(product);
+  }
+
+  async removeProduct(productId: number) {
+    const product = await this.findProductById(productId);
+
+    await this.productsRepo.remove(product);
   }
 }
