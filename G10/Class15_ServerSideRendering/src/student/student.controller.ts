2b7ecc88eeb1d3545253c633525c550946e55db0
Ivo Kostovski <ivo.kostovski@gmail.com>
2023-05-12 20:27:58 +0200

    Node ADV done!

--- G10/Class15_ServerSideRendering/src/student/student.controller.ts
+++ G10/Class15_ServerSideRendering/src/student/student.controller.ts
@@ -1,15 +1,65 @@
-import { Controller, Get, Render } from '@nestjs/common';
-import { StudentService } from './student.service';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Redirect,
+  Render,
+} from "@nestjs/common";
+import { StudentService } from "./student.service";
 
-@Controller('student')
+@Controller("student")
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
   @Get()
-  @Render('student/student-list')
+  @Render("student/student-list")
   async getStudents() {
     const students = this.studentService.getStudents();
 
     return { students };
+  }
+
+  @Get("add-student")
+  @Render("student/add-student")
+  getAddStudentForm() {
+    return {};
+  }
+
+  @Get("update-student/:id")
+  @Render("student/update-student")
+  getUpdateStudentForm(@Param("id") id: string) {
+    const student = this.studentService.getStudent(id);
+
+    return student;
+  }
+
+  @Get(":id")
+  @Render("student/student-details")
+  getStudent(@Param("id") id: string) {
+    const student = this.studentService.getStudent(id);
+
+    return student;
+  }
+
+  // to add the student
+  @Post()
+  @Redirect("/student")
+  createStudent(@Body() body: any) {
+    return this.studentService.createStudent(body);
+  }
+
+  // POST as we can't use PUT nor PATCH in form method attribute
+  @Post("update-student/:id")
+  @Redirect("/student")
+  updateStudent(@Body() body: any, @Param("id") id: string) {
+    return this.studentService.updateStudent(body, id);
+  }
+
+  @Get("delete-student/:id")
+  @Redirect("/student")
+  deleteStudent(@Param("id") id: string) {
+    return this.studentService.deleteStudent(id);
   }
 }
--- G10/Class15_ServerSideRendering/src/student/student.service.ts
+++ G10/Class15_ServerSideRendering/src/student/student.service.ts
@@ -1,15 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class StudentService {
+  public students = [
+    {
+      id: "abc213",
+      name: "Todor Pelivanov",
+      age: 30,
+      track: "NET",
+      isMale: true,
+    },
+    {
+      id: "qwe123",
+      name: "Daniela Danielovska",
+      age: 20,
+      track: "JS",
+      isMale: false,
+    },
+  ];
+
   getStudents() {
-    return [
-      {
-        id: 'abc213',
-        name: 'Todor Pelivanov',
-        age: 30,
-        track: 'NET',
-      },
-    ];
+    return this.students;
+  }
+
+  getStudent(id: string) {
+    return this.students.find(student => student.id === id);
+  }
+
+  createStudent(student: any) {
+    this.students.push(student);
+  }
+
+  updateStudent(updatedStudentData: any, studentId: string) {
+    const index = this.students.findIndex(student => student.id === studentId);
+
+    this.students[index] = {
+      ...updatedStudentData,
+      id: studentId,
+    };
+  }
+
+  deleteStudent(id: string) {
+    this.students = this.students.filter(student => student.id !== id);
   }
 }
